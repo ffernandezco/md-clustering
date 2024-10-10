c1f89712119ee9471b535803d1cd260d6e9d25e5
BuildTools <unconfigured@null.spigotmc.org>
2024-10-10 23:00:06 +0200

    Cambios de configuración y personalización, posibilidad de ejecutar con CUDA y seleccionar distintos batch_sizes y modelos

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import preprocess
+import vectorize
 
-# Ejecución del script
-input_file = 'data/DataI_MD.csv'  # Ruta al archivo CSV original
-output_file = 'data/DataI_MD_POST.csv'
+# ---- DESCOMENTAR PROCESOS QUE SE REQUIERAN ---
 
-preprocess.preprocess(input_file, output_file)
+# PREPROCESADO
+# input_file = 'data/DataI_MD.csv'  # Ruta al archivo CSV original
+# output_file = 'data/DataI_MD_POST.csv'
+#
+# preprocess.preprocess(input_file, output_file)
 
+# TOKENIZADO + VECTORIZACIÖN
+input_csv_path = 'data/DataI_MD_POST.csv'
+output_csv_path = 'data/DataI_MD_VECTOR.csv'
+
+vectorize.vectorize(input_csv_path, output_csv_path, True)
+# También se puede especificar el modelo (por defecto: AIDA-UPM/BERTuit-base),
+# from_tf (por defecto: True), la columna de procesado del csv de entrada (por defecto: 4)
+# y el batch_size para repartir la carga de trabajo (por defecto: 64)
