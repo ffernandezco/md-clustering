65dc7c543f0fce3bdded081b0254928044f93dd7
BuildTools <unconfigured@null.spigotmc.org>
2024-10-11 14:13:58 +0200

    Initial model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import preprocess
 import vectorize
+import model
 
 # ---- DESCOMENTAR PROCESOS QUE SE REQUIERAN ---
 
@@ -10,10 +11,13 @@ import vectorize
 # preprocess.preprocess(input_file, output_file)
 
 # TOKENIZADO + VECTORIZACIÖN
-input_csv_path = 'data/DataI_MD_POST.csv'
-output_csv_path = 'data/DataI_MD_VECTOR.csv'
+# input_csv_path = 'data/DataI_MD_POST.csv'
+# output_csv_path = 'data/DataI_MD_VECTOR.csv'
 
-vectorize.vectorize(input_csv_path, output_csv_path, True)
+# vectorize.vectorize(input_csv_path, output_csv_path, True)
 # También se puede especificar el modelo (por defecto: AIDA-UPM/BERTuit-base),
 # from_tf (por defecto: True), la columna de procesado del csv de entrada (por defecto: 4)
 # y el batch_size para repartir la carga de trabajo (por defecto: 64)
+
+# ENTRENAR MODELO
+model.train("data/VECTOR_BERTuit.csv")
