de67a550c520b3a74b9a9c9068e33466116b0cc9
Markel15 <103565976+Markel15@users.noreply.github.com>
2024-10-11 23:40:21 +0200

    Optimización (aprox. 6 mins)

--- model_opt.py
+++ model_opt.py
@@ -7,6 +7,7 @@ from multiprocessing import Pool
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.neighbors import NearestNeighbors
 
 
 def train(input_vector_path):
@@ -14,15 +15,16 @@ def train(input_vector_path):
     all_points = data.values
     n_points = len(all_points)
 
-    # Crear KDTree para búsqueda rápida de vecinos
-    tree = KDTree(all_points)
+    # Crear NearestNeighbors para búsqueda rápida de vecinos
+    neighbors_model = NearestNeighbors(radius=20, algorithm='auto', n_jobs=-1)
+    neighbors_model.fit(all_points)
 
     visited_points = np.zeros(n_points, dtype=bool)  # Para marcar los puntos visitados
-    near_points = defaultdict(set)
-    core_points = set()
-    border_points = set()
-    non_core_points = set()
-    clusters = {}
+    near_points = [set() for _ in range(n_points)]  # Lista de sets para near_points
+    core_points = np.zeros(n_points, dtype=bool)
+    border_points = np.zeros(n_points, dtype=bool)
+    non_core_points = np.zeros(n_points, dtype=bool)
+    clusters = np.full(n_points, -1, dtype=np.int32)
     min_distance = 20
     near_point_count = 5
     current_cluster_id = 0
@@ -31,24 +33,24 @@ def train(input_vector_path):
     for i in tqdm(range(n_points), desc="Identificando core points"):
         if not visited_points[i]:
             visited_points[i] = True
-            indices = tree.query_radius([all_points[i]], r=min_distance)[0]  # Vecinos dentro de min_distance
+            indices = neighbors_model.radius_neighbors([all_points[i]], return_distance=False)[0]  # Vecinos dentro de min_distance
             near_points[i] = set(indices) - {i}  # Remover el mismo punto
             if len(near_points[i]) >= near_point_count:
-                core_points.add(i)
+                core_points[i] = True
             else:
-                non_core_points.add(i)
+                non_core_points[i] = True
 
     # Expansión de los clusters
     visited_points.fill(False)  # Limpiar para usar en la expansión de los clusters
-    for i in tqdm(core_points, desc="Expandiendo clusters"):
-        if i not in clusters:  # Si no ha sido asignado a un cluster
+    for i in tqdm(np.where(core_points)[0], desc="Expandiendo clusters"):
+        if clusters[i] == -1:  # Si no ha sido asignado a un cluster
             current_cluster_id += 1
             expand_cluster(i, current_cluster_id, clusters, core_points, border_points, visited_points, near_points)
 
-    print("Clusters:", clusters)
-    print("Core Points:", core_points)
-    print("Border Points:", border_points)
-    print("Non-Core (Noise) Points:", non_core_points)
+    print("Número de clusters:", current_cluster_id)
+    print("Core Points:", np.sum(core_points))
+    print("Border Points:", np.sum(border_points))
+    print("Non-Core (Noise) Points:", np.sum(non_core_points))
     print("Finished")
 
     plot_clusters(all_points, clusters)
@@ -65,21 +67,18 @@ def expand_cluster(i, cluster_id, clusters, core_points, border_points, visited_
         if not visited_points[j]:
             visited_points[j] = True
             # Si es un core point
-            if j in core_points:
+            if core_points[j]:
                 # Agregar nuevos vecinos para visitar solo si no han sido visitados
                 to_visit.extend([neighbor for neighbor in near_points[j] if not visited_points[neighbor]])
                 clusters[j] = cluster_id
             else:
                 # Si no es core point, pero está dentro de un cluster, es un border point
-                if j not in clusters:
+                if clusters[j] == -1:
                     clusters[j] = cluster_id
-                    border_points.add(j)
+                    border_points[j] = True
 
 
 def plot_clusters(all_points, clusters):
-    # Convertir los clusters a un array numpy para indexado fácil
-    cluster_labels = np.array([clusters.get(i, -1) for i in range(len(all_points))])
-
     # Reducir las dimensiones a 2D si es necesario
     if all_points.shape[1] > 2:
         pca = PCA(n_components=2)
@@ -89,7 +88,7 @@ def plot_clusters(all_points, clusters):
 
     # Dibujar los puntos, coloreando según el cluster asignado
     plt.figure(figsize=(10, 7))
-    unique_clusters = set(cluster_labels)
+    unique_clusters = np.unique(clusters)
 
     for cluster_id in unique_clusters:
         if cluster_id == -1:
@@ -99,7 +98,7 @@ def plot_clusters(all_points, clusters):
             color = plt.cm.get_cmap('tab10')(cluster_id / len(unique_clusters))  # Colores para clusters
             label = f'Cluster {cluster_id}'
 
-        mask = (cluster_labels == cluster_id)
+        mask = (clusters == cluster_id)
         plt.scatter(all_points_2d[mask, 0], all_points_2d[mask, 1], s=50, c=[color], label=label, alpha=0.6)
 
     plt.title("Clusters Visualized")
@@ -111,4 +110,4 @@ def plot_clusters(all_points, clusters):
 
 
 
-# El cálculo de distancia ya no es necesario dado que usamos KDTree
+# El cálculo de distancia ya no es necesario dado que usamos NearestNeighbors
